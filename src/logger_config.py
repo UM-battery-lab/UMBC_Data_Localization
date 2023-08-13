1964441eb3daa80892937e5da9c87a251800c9bc
ZiyiLiu <97143566+YiLiiu@users.noreply.github.com>
2023-08-13 01:31:36 -0400

    Update logger with Singleton pattern

--- src/logger_config.py
+++ src/logger_config.py
@@ -1,6 +1,18 @@
 import logging
 
+class SingletonLogger:
+    _logger = None
+
+    @classmethod
+    def get_instance(cls):
+        if not cls._logger:
+            cls._logger = logging.getLogger(__name__)
+            cls._logger.setLevel(logging.INFO)
+            handler = logging.StreamHandler()
+            formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+            handler.setFormatter(formatter)
+            cls._logger.addHandler(handler)
+        return cls._logger
+
 def setup_logger():
-    logging.basicConfig(level=logging.INFO)
-    logger = logging.getLogger(__name__)
-    return logger
+    return SingletonLogger.get_instance()
