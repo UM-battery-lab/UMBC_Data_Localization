804bd9f7d0b0d4d8f89633304f1baa9b3e51d3e7
Jason Siegel <siegeljb@umich.edu>
2023-08-31 11:25:21 -0400

    updateg git ignore to skip pyc

--- src/config/path_config.py
+++ src/config/path_config.py
@@ -1,5 +1,5 @@
 import os
 
-ROOT_PATH = os.path.join(os.path.dirname(os.getcwd()), 'voltaiq_data')
-# ROOT_PATH = "/Volumes/me-battery-controls-lab/voltaiq_data"
+#ROOT_PATH = os.path.join(os.path.dirname(os.getcwd()), 'voltaiq_data')
+ROOT_PATH = r'/home/siegeljb/Lab_share_Volt/voltaiq_data'
 JSON_FILE_PATH = os.path.join(ROOT_PATH, 'directory_structure.json')
--- src/updatedb.py
+++ src/updatedb.py
@@ -1,8 +1,14 @@
+import sys
+#sys.path.insert(0, '~/Documents/UMBC_Data_Localization/src/')
+#sys.path.insert(0, '~/Documents/UMBC_Data_Localization/')
+#sys.path.insert(0, '../src')
+
+
 from src.model.DataManager import DataManager
 
 def update_db():
     dataManager = DataManager()
-    dataManager._updatedb(start_before='2023-06-30_23-59-59')
-
+    dataManager._updatedb()
+#start_before='2023-06-30_23-59-59'
 if __name__ == '__main__':
     update_db()
