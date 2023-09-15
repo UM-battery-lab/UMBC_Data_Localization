558230547bebb673ce74716303e439f49cf67edd
Jason Siegel <siegeljb@umich.edu>
2023-09-15 09:56:10 -0400

    updated integration test to cycle through cells

--- test/integration/integration_test.py
+++ test/integration/integration_test.py
@@ -23,6 +23,14 @@ def present_cell():
     presenter.attach(viewer)
 
     # test process_cell
-    cell_cycle_metrics, cell_data, cell_data_vdf, cell_data_rpt = dataManager.process_cell(cell_name)
+
+    #for i in range
+    for cell in range(62,80):
+        cell_name = "GMJuly2022_CELL"+f'{cell:03d}'
+        try:
+            cell_cycle_metrics, cell_data, cell_data_vdf, cell_data_rpt = dataManager.process_cell(cell_name)#, start_time='2023-07-01_00-00-00', end_time='2023-07-28_23-59-59')
+        except Exception as e:
+            print(e)
+
 if __name__ == '__main__':
     present_cell()
