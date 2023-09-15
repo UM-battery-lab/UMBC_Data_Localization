d73bbbc4713b71eeeacf60548bb7e2db785ef446
Jason Siegel <siegeljb@umich.edu>
2023-09-15 09:56:10 -0400

    updated integration test to cycle through cells

--- test/integration/integration_test.py
+++ test/integration/integration_test.py
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 import sys,os
 sys.path.append(os.path.dirname(os.path.abspath("__file__")))
 if os.name=="nt":
@@ -10,28 +11,53 @@ import sys
 #sys.path.insert(0, '/Users/yiliu/Documents/GitHub/UMBC_Data_Localization/UMBC_Data_Localization/src')
 #sys.path.insert(0, 'C:\\Users\\siegeljb\\Documents\\UMBC_Data_Localization\\src')
 from multiprocessing import Pool
+=======
+import sys
+#sys.path.insert(0, '/Users/yiliu/Documents/GitHub/UMBC_Data_Localization/UMBC_Data_Localization/src')
+sys.path.insert(0, 'C:\\Users\\siegeljb\\Documents\\UMBC_Data_Localization\\src')
+>>>>>>> a8350d1 (updated integration test to cycle through cells)
 
 from src.model.DataManager import DataManager
 from src.presenter.Presenter import Presenter 
 from src.viewer.Viewer import Viewer
 
 # Integration test
+<<<<<<< HEAD
 def present_cell(cell_num):
     cell_name="GMJuly2022_CELL"+f'{cell_num:03d}'
+=======
+def present_cell():
+>>>>>>> a8350d1 (updated integration test to cycle through cells)
     dataManager = DataManager(use_redis=False)
     def save_figs(figs, cell_name):
         dataManager.save_figs(figs, cell_name)
     presenter = Presenter()
     viewer = Viewer(call_back=save_figs)
+<<<<<<< HEAD
     cell_name = "GMJuly2022_CELL081"
+=======
+    #cell_name = "UMBL2022FEB_CELL152051"
+
+>>>>>>> a8350d1 (updated integration test to cycle through cells)
     dataManager.attach(presenter)
     presenter.attach(viewer)
 
     # test process_cell
+<<<<<<< HEAD
     try:
         cell_cycle_metrics, cell_data, cell_data_vdf, cell_data_rpt = dataManager.process_cell(cell_name)#, start_time='2023-07-01_00-00-00', end_time='2023-07-28_23-59-59')
     except Exception as e:
         print(e)
+=======
+
+    #for i in range
+    for cell in range(62,80):
+        cell_name = "GMJuly2022_CELL"+f'{cell:03d}'
+        try:
+            cell_cycle_metrics, cell_data, cell_data_vdf, cell_data_rpt = dataManager.process_cell(cell_name)#, start_time='2023-07-01_00-00-00', end_time='2023-07-28_23-59-59')
+        except Exception as e:
+            print(e)
+>>>>>>> a8350d1 (updated integration test to cycle through cells)
 
 if __name__ == '__main__':
 
