d98ccae8319f685c8742d5281fb791a410d4c7d9
Jason Siegel <siegeljb@umich.edu>
2023-09-25 14:40:50 -0400

    Fix after rebase integration test

--- test/integration/integration_test.py
+++ test/integration/integration_test.py
@@ -6,7 +6,7 @@ else:
     sys.path.append(os.path.dirname(os.path.abspath("__file__"))+"/src")
 print(sys.path)
 
-import sys
+#import sys
 #sys.path.insert(0, '/Users/yiliu/Documents/GitHub/UMBC_Data_Localization/UMBC_Data_Localization/src')
 #sys.path.insert(0, 'C:\\Users\\siegeljb\\Documents\\UMBC_Data_Localization\\src')
 from multiprocessing import Pool
@@ -29,14 +29,10 @@ def present_cell(cell_num):
     presenter.attach(viewer)
 
     # test process_cell
-
-    #for i in range
-    for cell in range(62,80):
-        cell_name = "GMJuly2022_CELL"+f'{cell:03d}'
-        try:
-            cell_cycle_metrics, cell_data, cell_data_vdf, cell_data_rpt = dataManager.process_cell(cell_name)#, start_time='2023-07-01_00-00-00', end_time='2023-07-28_23-59-59')
-        except Exception as e:
-            print(e)
+    try:
+        cell_cycle_metrics, cell_data, cell_data_vdf, cell_data_rpt = dataManager.process_cell(cell_name)#, start_time='2023-07-01_00-00-00', end_time='2023-07-28_23-59-59')
+    except Exception as e:
+        print(e)
 
 if __name__ == '__main__':
 
