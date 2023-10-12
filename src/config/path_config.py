5dba241d49bbe5ea898cd890e8f70802719a3dfd
Jason Siegel <siegeljb@umich.edu>
2023-10-12 15:57:51 -0400

    change to local directory

--- src/config/path_config.py
+++ src/config/path_config.py
@@ -2,7 +2,8 @@ import os
 
 #ROOT_PATH = os.path.join(os.path.dirname(os.getcwd()), 'voltaiq_data')
 # ROOT_PATH = "/Volumes/me-battery-controls-lab/voltaiq_data"
-ROOT_PATH = "V:\\voltaiq_data\\"
+# ROOT_PATH = "V:\\voltaiq_data\\"
+ROOT_PATH = r'/home/siegeljb/Lab_share_Volt/voltaiq_data'
 DIR_STRUCTURE_PATH = os.path.join(ROOT_PATH, 'directory_structure.json')
 PROJECT_DEVICES_PATH = os.path.join(ROOT_PATH, 'project_devices.json')
 WRONG_TR_NAME_PATH = os.path.join(ROOT_PATH, 'wrong_tr_name.json')
