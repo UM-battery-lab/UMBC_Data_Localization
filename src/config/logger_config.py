ae44e9ef595ee9b229b6a6775f43d19e847f5059
ZiyiLiu <97143566+YiLiiu@users.noreply.github.com>
2023-10-13 22:14:58 +0800

    Log file every day

--- src/config/logger_config.py
+++ src/config/logger_config.py
@@ -1,6 +1,7 @@
 import logging
+import datetime
 LOG_LEVEL = logging.INFO
 LOG_FORMAT = '%(asctime)s - %(name)s - %(levelname)s - %(message)s'
-#TODO: Change this path to the remote server
-LOG_FILE_PATH = 'logs/app.log' 
+today_str = datetime.date.today().strftime('%Y-%m-%d')
+LOG_FILE_PATH = f'logs/app_{today_str}.log'
 
