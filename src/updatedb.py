6f522f519d5bf9766216f1ccae1b62bbfb97b09b
Jason Siegel <siegeljb@umich.edu>
2023-10-13 12:13:40 -0400

    specific dev id

--- src/updatedb.py
+++ src/updatedb.py
@@ -3,6 +3,6 @@ from src.model.DataManager import DataManager
 def update_db():
     dataManager = DataManager()
     #dataManager._updatedb(project_name='GMFEB23S' ,start_before='2023-06-01_22-59-59')
-    dataManager._updatedb()
+    dataManager._updatedb(device_id=5837)
 if __name__ == '__main__':
     update_db()
