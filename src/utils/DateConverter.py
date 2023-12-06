0ec527786294131a24c3b85a7705a5250110cb61
Jason Siegel <siegeljb@umich.edu>
2023-12-06 14:46:29 -0500

    really fix ms timestamp issue

--- src/utils/DateConverter.py
+++ src/utils/DateConverter.py
@@ -8,8 +8,8 @@ class DateConverter(metaclass=SingletonMeta):
         self.DATE_FORMAT = DATE_FORMAT
 
     def _timestamp_to_datetime(self, t):
-      #  t = t/1000.0
-        return datetime.datetime.fromtimestamp(t, tz=self.TZ_INFO,unit="ms")
+        t = t/1000.0
+        return datetime.datetime.fromtimestamp(t, tz=self.TZ_INFO)
     
     def _timestamp_to_str(self, t):
         dt = self._timestamp_to_datetime(t)
@@ -18,13 +18,13 @@ class DateConverter(metaclass=SingletonMeta):
     def _str_to_timestamp(self, date_str):
         dt = datetime.datetime.strptime(date_str, self.DATE_FORMAT)
         dt = dt.replace(tzinfo=self.TZ_INFO)
-        timestamp = dt.timestamp() * 1000
+        timestamp = dt.timestamp() * 1000.0
         return timestamp
     
     def _datetime_to_timestamp(self, dt):
         if dt.tzinfo is None:
             dt = dt.astimezone(self.TZ_INFO)  # Convert to the desired timezone
-        timestamp = dt.timestamp() * 1000
+        timestamp = dt.timestamp() * 1000.0
         return timestamp
 
     def _str_to_datetime(self, date_str):
