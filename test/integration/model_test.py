b140f650d81ba52915a6aebbaa217672515650f0
Jason Siegel <siegeljb@umich.edu>
2024-02-06 21:31:43 -0500

    not important changes

--- test/integration/model_test.py
+++ test/integration/model_test.py
@@ -76,12 +76,17 @@ def process_cell_test():
     cell_names=['GMJuly2022_CELL085','GMJuly2022_CELL088','GMJuly2022_CELL044','GMJuly2022_CELL046']
     cell_names=['GMJuly2022_CELL029','GMJuly2022_CELL043']
     cell_names=['GMJuly2022_CELL043']
-    cell_names=['GMJuly2022_CELL054','GMJuly2022_CELL043','GMJuly2022_CELL029','GMJuly2022_CELL901REF']
+    cell_names=['GMJuly2022_CELL901REF','GMJuly2022_CELL029','GMJuly2022_CELL054','GMJuly2022_CELL043']
+    cell_names=['GMJuly2022_CELL015','GMJuly2022_CELL018','GMJuly2022_CELL017','GMJuly2022_CELL0101','GMJuly2022_CELL104']
+    cell_names=['GMJuly2022_CELL016','GMJuly2022_CELL014','GMJuly2022_CELL019','GMJuly2022_CELL024','GMJuly2022_CELL030','GMJuly2022_CELL035','GMJuly2022_CELL043','GMJuly2022_CELL101','GMJuly2022_CELL102','GMJuly2022_CELL103']
+    
+    cell_names=['GMJuly2022_CELL008','GMJuly2022_CELL024','GMJuly2022_CELL030','GMJuly2022_CELL035','GMJuly2022_CELL030','GMJuly2022_CELL035','GMJuly2022_CELL093']
+    cell_names=['GMJuly2022_CELL022']
     # cell_names=["GMJuly2022_CELL"+f'{cell_num:03d}' for cell_num in range(99,105) ]
     #cell_names=["UMBL2022FEB_CELL"+f'{cell_num:03d}' for cell_num in [152097,152091,152051,152087,152042,152094,152045,152054,152039,152065,152041,152046,152057,152078,152033,152088,152036,152084,152085,152047,152072,152048]]
     dataManager = DataManager(use_redis=False)
     def save_figs(figs, cell_name, time_name):
-        dataManager.save_figs(figs, cell_name, time_name, keep_open=True)
+        dataManager.save_figs(figs, cell_name, time_name, keep_open=False)
     presenter = Presenter()
     viewer = Viewer(call_back=save_figs)
     #cell_name = "UMBL2022FEB_CELL152051"
@@ -92,7 +97,7 @@ def process_cell_test():
         cell_cycle_metrics, cell_data, cell_data_vdf, cell_data_rpt = None, None, None, None
         # test process_cell
 
-        cell_cycle_metrics, cell_data, cell_data_vdf, cell_data_rpt = dataManager.process_cell(cell_name, reset=True);#, reset=True)#)
+        cell_cycle_metrics, cell_data, cell_data_vdf, cell_data_rpt = dataManager.process_cell(cell_name);#, reset=True)#)
    
     #    cell_cycle_metrics, cell_data, cell_data_vdf, cell_data_rpt = dataManager.process_cell(cell_name, reset=False, start_time='2022-09-01_00-00-00', end_time='2022-10-02_23-59-59');#, reset=True)#)
     #    cell_cycle_metrics, cell_data, cell_data_vdf, cell_data_rpt = dataManager.process_cell(cell_name, reset=True, start_time='2022-09-13_10-00-00', end_time='2022-09-24_10-00-00');#, reset=True)#)
@@ -102,12 +107,12 @@ def process_cell_test():
         # Returns the number of
         # objects it has collected
         # and deallocated
-        collected = gc.collect()
+        # collected = gc.collect()
         
-        # Prints Garbage collector 
-        # as 0 object
-        print("Garbage collector: collected",
-                "%d objects." % collected)
+        # # Prints Garbage collector 
+        # # as 0 object
+        # print("Garbage collector: collected",
+        #         "%d objects." % collected)
 
 
 
@@ -190,7 +195,7 @@ if __name__ == '__main__':
     #process_tr_test()
     consistency_test()
 #    onetest()
-    process_cell_test()
+    #process_cell_test()
 
     # read_csv_test()
     #update_cycle_stats_test()
