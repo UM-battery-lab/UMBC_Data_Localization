db7f1d7628126f4e409e1048ca4dfae89e201a82
Jason Siegel <siegeljb@umich.edu>
2024-03-12 12:14:57 -0400

    fix updatedb with new presenter

--- src/updatedb.py
+++ src/updatedb.py
@@ -1,7 +1,11 @@
 from src.model.DataManager import DataManager
+from src.presenter.Presenter import Presenter 
+from src.viewer.Viewer import Viewer
 
 def update_db():
-    dataManager = DataManager()
+    viewer = Viewer()
+    presenter = Presenter(viewer=viewer)
+    dataManager = DataManager(presenter=presenter) 
     #dataManager._updatedb(project_name='GMFEB23S' ,start_before='2023-06-01_22-59-59')
     #dataManager._updatedb(device_id=5837)
     dataManager._updatedb()
